5b8607ac7e3993788400e881ddabbdf06f7f8a54
Sherif Salama <80130495+sherow1982@users.noreply.github.com>
2025-11-02 02:59:14 +0200

    إضافة لودر صغير لاستدعاء سكربت تخصيص رسائل واتساب دون تعديل كبير على الصفحات

---
+++ assets/js/boot.js
@@ -0,0 +1,5 @@
+// تحميل سكربت تخصيص واتساب بدون تعطيل البنية
+(function(){
+  function load(src){var s=document.createElement('script');s.src=src;s.defer=true;document.head.appendChild(s)}
+  load('assets/js/wa-intl.js');
+})();
